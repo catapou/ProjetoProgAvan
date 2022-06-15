a4d4322a251ab125e7b33244a82c7de44a38565e
Gustavo Arroja <arrojag@gmail.com>
2022-06-15 01:44:37 +0100

    adição de todas as tabelas do modelo ER

---
+++ app/src/main/java/pt/ipg/SpotifyV2/TabelaBDPlaylists_Musicas.kt
@@ -0,0 +1,17 @@
+package pt.ipg.SpotifyV2
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDPlaylists_Musicas(db: SQLiteDatabase) : TabelaBD(db, NOME) {
+    override fun cria() {
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL)")
+    }
+
+    companion object {
+        const val NOME = "categorias"
+        const val CAMPO_NOME = "nome"
+
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME)
+    }
+}
