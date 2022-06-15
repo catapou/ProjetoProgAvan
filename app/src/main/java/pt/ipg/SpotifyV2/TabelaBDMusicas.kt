a4eafa46f4bdc47fe1e6d1bb37f260f71038b361
Gustavo Arroja <arrojag@gmail.com>
2022-06-15 01:57:58 +0100

    codigo da tabela de musicas

--- app/src/main/java/pt/ipg/SpotifyV2/TabelaBDMusicas.kt
+++ app/src/main/java/pt/ipg/SpotifyV2/TabelaBDMusicas.kt
@@ -5,13 +5,18 @@ import android.provider.BaseColumns
 
 class TabelaBDMusicas(db: SQLiteDatabase) : TabelaBD(db, NOME) {
     override fun cria() {
-        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL)")
+        db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, $CAMPO_NOME TEXT NOT NULL" +
+                ", $NOME_ALBUM TEXT NOT NULL, $DURACAO REAL NOT NULL, $NOME_DE_MUSICA TEXT NOT NULL, $DATA_DE_LANCAMENTO INTERGER NOT NULL)")
     }
 
     companion object {
         const val NOME = "Musicas"
-        const val CAMPO_NOME = "nome"
+        const val CAMPO_NOME = "Nome_autor"
+        const val NOME_ALBUM = "nome_album"
+        const val DURACAO = "duração"
+        const val NOME_DE_MUSICA = "nome_de_musica"
+        const val DATA_DE_LANCAMENTO = "data_de_lançamento"
 
-        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME)
+        val TODAS_COLUNAS = arrayOf(BaseColumns._ID, CAMPO_NOME, NOME_ALBUM, DURACAO, NOME_DE_MUSICA, DATA_DE_LANCAMENTO)
     }
 }
